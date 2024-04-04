66386998deb002328b41d6d853224554e7c60360
Julu1994 <mrjewel837@gmail.com>
2024-04-04 20:39:20 +0200

    exported the hooks to build and test

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import { babel } from '@rollup/plugin-babel';
-import { terser } from '@rollup/plugin-terser';
+import terser from '@rollup/plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import useDataFetch from './hooks/useDataFetch'
+import useCurrentWindowSize from './hooks/useCurrentWindowSize'
+import usePersistedState from './hooks/usePersistedState'
+
+export { useDataFetch, useCurrentWindowSize, usePersistedState }
