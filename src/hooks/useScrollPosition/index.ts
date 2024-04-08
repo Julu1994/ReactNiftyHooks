7a6840089b34ce853136a3a25b108dfa106e5b62
Julu1994 <mrjewel837@gmail.com>
2024-04-08 21:19:11 +0200

    exported the useScrollPosition hook

---
+++ src/hooks/useScrollPosition/index.ts
@@ -0,0 +1,2 @@
+import { useScrollPosition } from './useScrollPosition'
+export default useScrollPosition
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import useCurrentWindowSize from './hooks/useCurrentWindowSize'
 import usePersistedState from './hooks/usePersistedState'
 import useOutsideClick from './hooks/useOutsideClick'
 import useForm from './hooks/useForm'
+import useScrollPosition from './hooks/useScrollPosition'
 
 export {
   useDataFetch,
   useCurrentWindowSize,
   usePersistedState,
   useOutsideClick,
-  useForm
+  useForm,
+  useScrollPosition
 }
