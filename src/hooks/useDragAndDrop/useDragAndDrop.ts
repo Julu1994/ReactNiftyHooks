44ddc05880cf75dc3679f4be888bc7958aadd359
Julu1994 <mrjewel837@gmail.com>
2024-04-12 13:39:54 +0200

    added the missing tsdoc in the drag and drop hook

--- src/hooks/useDragAndDrop/useDragAndDrop.ts
+++ src/hooks/useDragAndDrop/useDragAndDrop.ts
@@ -1,6 +1,15 @@
 import { useState, useCallback } from 'react'
 import { DragState } from './type'
 
+/**
+ * A custom React hook that facilitates drag-and-drop operations.
+ * It tracks the dragging state and the data being dragged.
+ *
+ * @typeparam T The type of the data involved in the drag-and-drop operation.
+ * @returns An object containing the drag state, and functions to start and end the drag.
+ *
+ **/
+
 export function useDragAndDrop<T>() {
   const [dragState, setDragState] = useState<DragState<T>>({
     isDragging: false,
